26b5a225288c7450116d1f9ef6b1a95692936d79
harsh <harshranjan1995@gmail.com>
2020-10-03 19:37:13 +0530

    Updates

---
+++ precache-manifest.de03854b28339fe50f050a52da05b0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd9bf5cc92f516093c3d0f2548e7ed7",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "b144915728e8a622964a",
+    "url": "/netflix-clone/static/css/main.9df96428.chunk.css"
+  },
+  {
+    "revision": "a7f3cf3538b74363cc17",
+    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b144915728e8a622964a",
+    "url": "/netflix-clone/static/js/main.b7f77e14.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
