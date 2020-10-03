7b540e9f6656694300110bfb5ef6b77410295ee4
harsh <harshranjan1995@gmail.com>
2020-10-03 22:52:21 +0530

    Updates

--- precache-manifest.cb410091dd43a46babca9b7c049c7336.js
+++ precache-manifest.cb410091dd43a46babca9b7c049c7336.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7977cbbee805ad1868de6d4478a57e41",
+    "revision": "4b9ccd5acf731a1ef13c91b96f45badd",
     "url": "/netflix-clone/index.html"
   },
   {
-    "revision": "e7e305ea4d558b0fce11",
+    "revision": "071a4cc6efcc421fc25d",
     "url": "/netflix-clone/static/css/main.9df96428.chunk.css"
   },
   {
-    "revision": "a7f3cf3538b74363cc17",
-    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js"
+    "revision": "f4c2f9da280785cfe158",
+    "url": "/netflix-clone/static/js/2.e2e0e229.chunk.js"
   },
   {
     "revision": "894f61387b383e6e2e0543fb120b4656",
-    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js.LICENSE.txt"
+    "url": "/netflix-clone/static/js/2.e2e0e229.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e7e305ea4d558b0fce11",
-    "url": "/netflix-clone/static/js/main.40c653aa.chunk.js"
+    "revision": "071a4cc6efcc421fc25d",
+    "url": "/netflix-clone/static/js/main.6553d4f7.chunk.js"
   },
   {
     "revision": "42ccba1c2e183f5632cb",
